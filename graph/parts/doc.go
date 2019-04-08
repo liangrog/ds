d3a805f53013cb489d1618e671f9f40372d8e628
Roger Liang <pinguroger@gmail.com>
2019-04-08 02:01:09 +0000

    test

--- graph/parts/doc.go
+++ graph/parts/doc.go
@@ -1,6 +1,1 @@
-/*
-This package contains parts that make of a graph such as vertice and edge. It also defined some
-base interfaces that will allow extensibilities.
-*/
-
 package parts
--- graph/parts/graph.go
+++ graph/parts/graph.go
@@ -1,3 +1,7 @@
+/*
+This package contains parts that make of a graph such as vertice and edge. It also defined some
+base interfaces that will allow extensibilities.
+*/
 package parts
 
 import (
